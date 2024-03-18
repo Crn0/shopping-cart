c61a76d7cc8262cd442ac1abda5c88d5e4ff5e56
Crno0 <123456789+odin@users.noreply.github.com>
2024-03-18 16:53:45 +0800

    Add image and links element

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,7 +1,31 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import styles from "./css/navigation.module.css";
 
-function Navigation() {
+function Navigation({cartList}) {
 
-    return <p>Navigation</p>
+    return(
+        <header>
+            <nav className={`${styles.flex} ${styles.childPos}`}>
+                <div className={`${styles.flex}`}>
+                    <img src="https://placehold.co/100" alt="place holder image" />
+                </div>
+                <div className={`${styles.flex} ${styles.gap}`}>
+                    <Link to="home">Home</Link>
+                    <Link to="shop">Shop</Link>
+                    <Link to="cart">Cart <span>{cartList}</span></Link>
+                </div>
+            </nav>
+        </header>
+    );
 }
+
+Navigation.defaultProps = {
+    cartList: 0,
+};
+
+Navigation.propTypes = {
+    cartList: PropTypes.number
+};
 
 export default Navigation;
