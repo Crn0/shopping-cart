f7c8e1cfa7eb29ff7140b28bfb7e9cfed7d1bc4a
Crno0 <123456789+odin@users.noreply.github.com>
2024-03-18 16:48:27 +0800

    Add products logic

--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -1,7 +1,38 @@
+import { useOutletContext, Link, useLocation} from "react-router-dom";
+import ProductCard from "../product-card/product-card";
+import styles from "./css/shop.module.css"
+
+
 
 function Shop() {
+    const {products, setProducts} = useOutletContext();
+    const locationId = Number(useLocation().pathname?.replace(/\/[A-Za-z]{1,10}\//g, ""));
 
-    return <p>Shop</p>
+    return (
+        <main>
+            {locationId ? <ProductCard 
+                                products={products}
+                                setProducts={setProducts}
+                          /> 
+                        : (
+                            <div>
+                                <p>hello</p>
+                                {products?.map?.((product) => {
+                                    return (
+                                        <div key={product.id}>
+                                          <img src={product.images[0]} alt={product.title}/>
+                                          <span>{product.title}</span>
+                                          <span>{product.price}</span>
+                                          <Link to={`${product.id}`}>details</Link>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        )
+            }
+                         
+        </main>
+    );
 }
 
 export default Shop;
