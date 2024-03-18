66f15af1bea57327d3f08746222eb197f158d041
Crno0 <123456789+odin@users.noreply.github.com>
2024-03-18 16:48:44 +0800

    Create product cards

---
+++ src/components/product-card/product-card.jsx
@@ -0,0 +1,111 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { useLocation, Link } from "react-router-dom";
+import styles from "./css/product-card.module.css";
+
+function ProductCard({products, setProducts})  {
+    const [index, setIndex] = useState(0);
+    const id = useLocation().pathname?.replace(/\/[A-Za-z]{1,10}\//g, "");
+    const product = products?.find?.((product) => product.id === +id);
+
+    const handleAddToCart = (e) => {
+        e.preventDefault();
+        const form = e.target
+        const formData = new FormData(form);
+        const formJson = Object.fromEntries(formData.entries());
+       
+        // console.log(formJson.selectProduct);
+
+        const newProduct = products.map((product) => {
+            if(product.id === +id) {
+                
+                return {...product, bought: true, quantity: +product?.quantity + +formJson.selectProduct};
+            }
+
+            return product
+        });
+        
+        setProducts(newProduct)
+        
+        return;
+    };
+    console.log(product)
+    const handleImageChange = (e) => {
+        const parent = e.target.parentElement;
+        const newIndex = parent.dataset.index;
+        
+        setIndex(+newIndex);
+    };
+
+    return (
+        <div>
+            {/* button image and main image container */}
+           
+            <div>
+                <div>
+                    {product?.images.map?.((image, index) => {
+                        return (
+                            <button 
+                                key={image}
+                                data-index={`${index}`} 
+                                onClick={(e) => handleImageChange(e)}
+                            >
+                                <img 
+                                    src={image} 
+                                    alt={`${product.title}${index}`} 
+                                />
+                            </button>
+                        );
+                    })}
+                </div>
+                    {/* product image */}
+                <div>
+                    <img 
+                        src={`${product?.images[index]}`} 
+                        alt={product?.title} 
+                    />
+                </div>
+            </div>
+
+            {/* name, price description container */}
+            <div>
+                <h3>{product?.title}</h3>
+                <p>{`price: $${product?.price}`}</p>
+                <p>{`description: ${product?.description}`}</p>
+            </div>
+
+            {/* form container */}
+            <div>
+                <form method="post" onSubmit={handleAddToCart}>
+                    <label>
+                        <span>Quantity*</span>
+                        <select 
+                            name="selectProduct" 
+                            defaultValue={"1"}
+                        >
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+                    </label>
+
+                    <button type="submit">Add to cart</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+ProductCard.propTypes = {
+    products: PropTypes.array,
+    setProducts: PropTypes.func,
+};
+
+export default ProductCard;
