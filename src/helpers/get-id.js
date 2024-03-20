2f894a2815ce2d766a7607f469910fcc06d502e4
Crno0 <123456789+odin@users.noreply.github.com>
2024-03-20 19:06:05 +0800

    Change regex

--- src/helpers/get-id.js
+++ src/helpers/get-id.js
@@ -1,5 +1,6 @@
 function GetId(cb) {
-    return Number(cb?.().pathname.replace(/\/[A-Za-z]{1,10}\//g, ""));
+    // accept a useLocation API as a callback to get the id
+    return Number(cb?.().pathname.replace(/\/[A-Za-z]{1,}\//g, ""));
 }
 
 export default GetId;
