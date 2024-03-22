206e16f23a3dd5485dc73b051bce76d87fa3f9c8
Crno0 <123456789+odin@users.noreply.github.com>
2024-03-22 13:55:16 +0800

    Mock data

---
+++ test/data/products.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        title: "foo",
+        category: "mens-watches",
+        price: 120,
+        images: [
+            "https://placehold.co/100",
+            "https://placehold.co/200",
+            "https://placehold.co/300",
+        ],
+        id: 1,
+    },
+    {
+        title: "bar",
+        category: "womens-watches",
+        price: 120,
+        images: [
+            "https://placehold.co/100",
+            "https://placehold.co/200",
+            "https://placehold.co/300",
+        ],
+        id: 2,
+    },
+];
+
+export default products;
