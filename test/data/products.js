531a8bc488efddb179b7306bf15efe79bf49f3a5
Crno0 <123456789+odin@users.noreply.github.com>
2024-03-23 16:02:46 +0800

    Add quantity and bought keys

--- test/data/products.js
+++ test/data/products.js
@@ -3,6 +3,8 @@ const products = [
         title: "foo",
         category: "mens-watches",
         price: 120,
+        quantity: 42,
+        bought: false,
         images: [
             "https://placehold.co/100",
             "https://placehold.co/200",
@@ -11,9 +13,24 @@ const products = [
         id: 1,
     },
     {
+        title: "baz",
+        category: "mens-watches",
+        price: 120,
+        quantity: 0,
+        bought: false,
+        images: [
+            "https://placehold.co/100",
+            "https://placehold.co/200",
+            "https://placehold.co/300",
+        ],
+        id: 3,
+    },
+    {
         title: "bar",
         category: "womens-watches",
         price: 120,
+        quantity: 0,
+        bought: false,
         images: [
             "https://placehold.co/100",
             "https://placehold.co/200",
