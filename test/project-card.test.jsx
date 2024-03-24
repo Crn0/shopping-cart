519896816bd82b2a0b5a2ebc1c1dc1606e8f9e79
Crno0 <123456789+odin@users.noreply.github.com>
2024-03-24 13:58:55 +0800

    Use router to test

--- test/project-card.test.jsx
+++ test/project-card.test.jsx
@@ -1,45 +1,78 @@
+import { RouterProvider, createMemoryRouter} from "react-router-dom";
 import { vi } from "vitest"
 import { render, screen} from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
 import products from "./data/products";
+import App from "../src/App";
+import Shop from "../src/components/shop/shop";
 import ProductCard from "../src/components/product-card/product-card";
 
 const productId = products[0].id;
 const setProducts = vi.fn();
 
-const setup = (jsx) => {
+vi.mock("../src/hooks/useProduct", () => {
+    return {
+        default: () => {
+            return {
+                products: products,
+                setProducts: setProducts,
+                error: null,
+                loading: false,
+        }
+        }
+    }
+});
+
+const setup = (router) => {
     return {
         user: userEvent.setup(),
-        ...render(jsx)
+        ...render(<RouterProvider router={router} />)
     }
 };
 
 describe("ProductCard component", () => {
+    const routes = [
+        {
+            path: "/",
+            element: <App />,
+            children: [
+                {
+                    path: "shop",
+                    element: <Shop />,
+                    children: [
+                        {
+                            path: ":productId",
+                            element: <ProductCard />
+                        }
+                    ],
+                    
+                }
+               
+            ],
+        }
+    ];
 
     it("renders the product images", () => {
-        render(
-            <ProductCard 
-                products={products}
-                productId={productId}
-                setProducts={setProducts}
-            />
-        );
+        const router = createMemoryRouter(routes, {
+            initialEntries: ["/",  "/shop", `/shop/${productId}`],
+            initialIndex: 3,
+        });
+
+        render(<RouterProvider router={router} />);
 
         for (let i = 0; i < products[0].images.length; i += 1) {
             
             expect(screen.getByRole("img", { name: `foo${i}`})).toBeInTheDocument();
-        } 
+        }
     });
 
     it("select the correct quantity", async () => {
-        const { user } = setup(
-            <ProductCard 
-                products={products}
-                productId={productId}
-                setProducts={setProducts}
-            />
-        );
+        const router = createMemoryRouter(routes, {
+            initialEntries: ["/",  "/shop", `/shop/${productId}`],
+            initialIndex: 3,
+        });
+        const { user } = setup(router);
 
         const select = screen.getByLabelText("Quantity*");
         const option = screen.getByRole("option", { name: "10"})
@@ -57,13 +90,11 @@ describe("ProductCard component", () => {
     });
 
     it("add to cart", async () => {
-        const { user } = setup(
-            <ProductCard 
-                products={products}
-                productId={productId}
-                setProducts={setProducts}
-            />
-        );
+        const router = createMemoryRouter(routes, {
+            initialEntries: ["/",  "/shop", `/shop/${productId}`],
+            initialIndex: 3,
+        });
+        const { user } = setup(router);
 
         const button = screen.getByRole("button", { name: "Add to cart"});
 
